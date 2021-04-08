55b348722946242b7a95ba1cba2268f3724f0df1
Luis Angel <lanquidi1@hotmail.com>
2021-04-08 15:18:15 -0500

    Se sube la aplicacion api para administrar tienda virtual

---
+++ index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const port = process.env.PORT || 3977;
+const {
+  API_VERSION,
+  IP_SERVER,
+  PORT_DB,
+  USER_DB,
+  PASSWORD_DB,
+} = require("./config");
+const urlDatabaseProd = `mongodb+srv://${USER_DB}:${PASSWORD_DB}@cluster0-2dg8m.mongodb.net/nava?retryWrites=true&w=majority`;
+// const urlDatabaseDev = `mongodb://${IP_SERVER}:${PORT_DB}/webpersonal`;
+
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(
+  urlDatabaseProd,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  },
+  (err, res) => {
+    if (err) throw err;
+
+    console.log("La conexión a la base de datos es correcta.");
+
+    app.listen(port, () => {
+      console.log("####################");
+      console.log("##### API REST #####");
+      console.log("####################");
+      console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}`);
+    });
+  }
+);
