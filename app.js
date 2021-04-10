749a0c46dcb34b952ada96d309a7ce2050ab3ca7
Luis Angel <lanquidi1@hotmail.com>
2021-04-10 13:35:55 -0500

    Se sube app a heroku

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-// Configure Header Http
+// Configure Header Https
 // .....
 
 // Router Basic
